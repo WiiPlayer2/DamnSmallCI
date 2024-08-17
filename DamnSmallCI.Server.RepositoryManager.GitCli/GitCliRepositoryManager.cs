c6db65dfd76675c2f7b70d458a31c9cfadf358b0
Waldemar Tomme <waldemar.tomme@bluehands.de>
2024-08-17 17:48:33 +0200

    Implement repository manager

--- DamnSmallCI.Server.RepositoryManager.GitCli/GitCliRepositoryManager.cs
+++ DamnSmallCI.Server.RepositoryManager.GitCli/GitCliRepositoryManager.cs
@@ -1,11 +1,18 @@
+using CliWrap;
 using DamnSmallCI.Server.Application;
 using DamnSmallCI.Server.Domain;
-using LanguageExt;
 using LanguageExt.Effects.Traits;
 
 namespace DamnSmallCI.Server.RepositoryManager.GitCli;
 
 internal class GitCliRepositoryManager<RT> : IRepositoryManager<RT> where RT : struct, HasCancel<RT>
 {
-    public Aff<RT, Unit> Clone(RepositoryInfo repositoryInfo, DirectoryInfo directory) => throw new NotImplementedException();
+    public Aff<RT, Unit> Clone(RepositoryInfo repositoryInfo, DirectoryInfo directory) =>
+        from _10 in Aff(async (RT rt) => await Cli.Wrap("git")
+            .WithArguments(["clone", repositoryInfo.Url.ToString(), directory.FullName])
+            .ExecuteAsync(rt.CancellationToken))
+        from _20 in Aff(async (RT rt) => await Cli.Wrap("git")
+            .WithArguments(["-C", directory.FullName, "checkout", repositoryInfo.CommitHash.Value])
+            .ExecuteAsync(rt.CancellationToken))
+        select unit;
 }
