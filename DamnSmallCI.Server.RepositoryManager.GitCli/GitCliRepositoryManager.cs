d2ba81e95df6a26926b7801c7758a0807607e684
Waldemar Tomme <waldemar.tomme@bluehands.de>
2024-08-17 18:01:00 +0200

    Use guards instead of exceptions for git commands

--- DamnSmallCI.Server.RepositoryManager.GitCli/GitCliRepositoryManager.cs
+++ DamnSmallCI.Server.RepositoryManager.GitCli/GitCliRepositoryManager.cs
@@ -1,6 +1,8 @@
 using CliWrap;
+using CliWrap.Buffered;
 using DamnSmallCI.Server.Application;
 using DamnSmallCI.Server.Domain;
+using LanguageExt.Common;
 using LanguageExt.Effects.Traits;
 
 namespace DamnSmallCI.Server.RepositoryManager.GitCli;
@@ -8,11 +10,15 @@ namespace DamnSmallCI.Server.RepositoryManager.GitCli;
 internal class GitCliRepositoryManager<RT> : IRepositoryManager<RT> where RT : struct, HasCancel<RT>
 {
     public Aff<RT, Unit> Clone(RepositoryInfo repositoryInfo, DirectoryInfo directory) =>
-        from _10 in Aff(async (RT rt) => await Cli.Wrap("git")
+        from cloneResult in Aff(async (RT rt) => await Cli.Wrap("git")
             .WithArguments(["clone", repositoryInfo.Url.ToString(), directory.FullName])
-            .ExecuteAsync(rt.CancellationToken))
-        from _20 in Aff(async (RT rt) => await Cli.Wrap("git")
+            .WithValidation(CommandResultValidation.None)
+            .ExecuteBufferedAsync(rt.CancellationToken))
+        from _10 in guard(cloneResult.IsSuccess, Error.New($"Clone failed: {cloneResult.StandardError}"))
+        from checkoutResult in Aff(async (RT rt) => await Cli.Wrap("git")
             .WithArguments(["-C", directory.FullName, "checkout", repositoryInfo.CommitHash.Value])
-            .ExecuteAsync(rt.CancellationToken))
+            .WithValidation(CommandResultValidation.None)
+            .ExecuteBufferedAsync(rt.CancellationToken))
+        from _30 in guard(checkoutResult.IsSuccess, Error.New($"Checkout failed: {checkoutResult.StandardError}"))
         select unit;
 }
