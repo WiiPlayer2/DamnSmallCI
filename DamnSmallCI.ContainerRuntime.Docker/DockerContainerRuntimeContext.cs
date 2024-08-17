39178641cbfc1a894f1b3ea4f444768107f648cc
Waldemar Tomme <waldemar.tomme@bluehands.de>
2024-08-17 18:33:48 +0200

    Pull image before creating container in docker

--- DamnSmallCI.ContainerRuntime.Docker/DockerContainerRuntimeContext.cs
+++ DamnSmallCI.ContainerRuntime.Docker/DockerContainerRuntimeContext.cs
@@ -13,21 +13,7 @@ internal class DockerContainerRuntimeContext<RT>(DockerClient client, VolumeResp
     public async ValueTask DisposeAsync() => await client.Volumes.RemoveAsync(volume.Name);
 
     public Aff<RT, Unit> CopyFilesFromDirectory(DirectoryInfo directory) =>
-        from container in Aff((RT rt) => client.Containers.CreateContainerAsync(new CreateContainerParameters() // TODO dispose container when done
-        {
-            Image = "ubuntu",
-            WorkingDir = "/src",
-            AttachStdin = true,
-            Tty = true,
-            Volumes = new Dictionary<string, EmptyStruct>
-            {
-                {$"{volume.Name}:/src", default},
-            },
-            HostConfig = new HostConfig
-            {
-                AutoRemove = true,
-            },
-        }, rt.CancellationToken).ToValue())
+        from container in CreateContainer(ImageName.From("busybox")) // TODO dispose container when done
         let tarStream = new MemoryStream()
         from _10 in Aff((RT rt) => TarFile.CreateFromDirectoryAsync(directory.FullName, tarStream, false, rt.CancellationToken).ToUnit().ToValue())
         from _20 in Eff(fun(() => tarStream.Position = 0))
@@ -38,6 +24,23 @@ internal class DockerContainerRuntimeContext<RT>(DockerClient client, VolumeResp
         select unit;
 
     public Aff<RT, IContainer<RT>> NewContainer(ImageName image) =>
+        from container in CreateContainer(image)
+        from containerStarted in Aff((RT rt) => client.Containers.StartContainerAsync(container.ID, new ContainerStartParameters(), rt.CancellationToken).ToValue())
+        from _10 in guard(containerStarted, Error.New("Failed to start container"))
+        select (IContainer<RT>) new DockerContainer<RT>(client, container.ID);
+
+    private Aff<RT, CreateContainerResponse> CreateContainer(ImageName image) =>
+        from repoAndTag in SuccessEff(ParseImage(image))
+        from _10 in Aff((RT rt) => client.Images.CreateImageAsync(new ImagesCreateParameters
+                {
+                    FromImage = repoAndTag.Repo,
+                    Tag = repoAndTag.Tag,
+                },
+                new AuthConfig(),
+                new Progress<JSONMessage>(),
+                rt.CancellationToken)
+            .ToUnit()
+            .ToValue())
         from container in Aff((RT rt) => client.Containers.CreateContainerAsync(new CreateContainerParameters
         {
             Image = image.Value,
@@ -53,7 +56,14 @@ internal class DockerContainerRuntimeContext<RT>(DockerClient client, VolumeResp
                 AutoRemove = true,
             },
         }, rt.CancellationToken).ToValue())
-        from containerStarted in Aff((RT rt) => client.Containers.StartContainerAsync(container.ID, new ContainerStartParameters(), rt.CancellationToken).ToValue())
-        from _10 in guard(containerStarted, Error.New("Failed to start container"))
-        select (IContainer<RT>) new DockerContainer<RT>(client, container.ID);
+        select container;
+
+    // TODO should be part of the domain logic
+    private (string Repo, string Tag) ParseImage(ImageName imageName)
+    {
+        var splitIndex = imageName.Value.LastIndexOf(':');
+        return splitIndex == -1
+            ? (imageName.Value, "latest")
+            : (imageName.Value[..splitIndex], imageName.Value[(splitIndex + 1)..]);
+    }
 }
